7c93bc637c558a2f7b1556d2b5fcfb3425344e78
son.ho463 <son.ho463@gmail.com>
2020-05-24 01:00:16 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,15 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
 
     # debugger
 
-    if user && user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       #ログイン後にユーザー情報ページへリダイレクト
-      log_in user
-      redirect_to user
+      log_in @user
+       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
       # debugger
     else
       #エラーメッセージを作成（flash)
@@ -20,7 +21,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
